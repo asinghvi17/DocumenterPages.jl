5dc60b646105276aa3a767a61a7f8667f10e5e6e
Anshul Singhvi <anshulsinghvi@gmail.com>
2025-01-30 11:46:28 -0500

    prepare for release

--- src/pagenode.jl
+++ src/pagenode.jl
@@ -1,4 +1,29 @@
+"""
+    PageNode(input; visible, collapsed)
+    PageNode(this_page, children; visible, collapsed)
 
+Creates a `PageNode` object, which may have some children.
+
+`input` can be anything that `pages` accepts in `Documenter.makedocs`,
+for example a string, a pair of strings, a vector of strings or pairs, or any combination thereof.
+
+You can also specify details for the toplevel page and its children separately in the two-argument constructor.
+
+## Examples
+
+```julia
+PageNode("index.md")
+```
+
+```julia
+PageNode("Home" => "index.md")
+```
+
+```julia
+PageNode(["index.md", "gallery.md"])
+```
+
+"""
 struct PageNode
     # page contents
     page::Union{String, Nothing}
